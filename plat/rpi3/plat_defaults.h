4f2b8a1804f968dfa13aa9f673c477d153c4ec72
Lukas Graber <lukas.graber@hensoldt-cyber.de>
2021-09-20 12:04:30 +0200

    [FOR] SEOS-2347: Fix alignment

--- plat/rpi3/plat_defaults.h
+++ plat/rpi3/plat_defaults.h
@@ -7,17 +7,15 @@
 
 #pragma once
 
-#define SDHC1_PADDR 0x3f300000
-#define SDHC1_SIZE  0x1000
-#define SDHC1_IRQ   126
+#define SDHC1_PADDR     0x3f300000
+#define SDHC1_SIZE      0x1000
+#define SDHC1_IRQ       126
 
-#define MAILBOX_PADDR 0x3f00b000
-#define MAILBOX_SIZE  0x1000
+#define MAILBOX_PADDR   0x3f00b000
+#define MAILBOX_SIZE    0x1000
 
-#define GPIO_PADDR 0x3f200000
-#define GPIO_SIZE  0x1000
-
-#define RASPPI          3
+#define GPIO_PADDR      0x3f200000
+#define GPIO_SIZE       0x1000
 
 // Set the default port to SDHC1.
 #define SdHostController_HW_INSTANCE_CONFIGURE_BY_DEFAULT(_inst_) \
